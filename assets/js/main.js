c277ecfcb37e965e9e034386922e00c9c33ba993
trxsh1_dev <yagosousa630@gmail.com>
2022-11-29 15:36:43 -0300

    Refatorando addEventListener

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,57 +1,63 @@
 const clock = document.querySelector('.timing');
 const start = document.querySelector('.start');
-const stops = document.querySelector('.stop');
+const stop1 = document.querySelector('.stop');
 const reset = document.querySelector('.reset');
 const restart = document.querySelector('.restart');
 let seconds = 0;
 let timer;
 
-start.addEventListener('click', function(e) {
-    clock.classList.add('started');
-    clearInterval(timer)
-    startClock();
-})
-stops.addEventListener('click', function(e) {
+stop1.addEventListener('click', function(e) {
     clock.classList.remove('started');
     clock.classList.add('paused');
     clearInterval(timer)
     pauseClock();
 })
-reset.addEventListener('click', function(e) {
-    clock.classList.remove('started');
-    clock.classList.remove('paused');
-    clearInterval(timer)
-    resetClock();
-})
-restart.addEventListener('click', function(e) {
-    clock.classList.add('started');
-    clearInterval(timer);
-    restartFunc();
+
+document.addEventListener('click',function(e) {
+    const el = e.target;
+    if(el.classList.contains('start')){
+        clock.classList.add('started');
+        clearInterval(timer)
+        startClock();
+    };
+    if(el.classList.contains('reset')){
+        clock.classList.remove('started');
+        clock.classList.remove('paused');
+        clearInterval(timer)
+        resetClock();
+    };
+    if(el.classList.contains('restart')){
+        clock.classList.add('started');
+        clearInterval(timer);
+        restartFunc();
+    };
 });
+
 function createSeconds(sec) {
     const date = new Date(sec * 1000);
-
     return date.toLocaleTimeString('pt-BR', {
         hour12: false,
         timeZone: "UTC",
     });      
-}
+};
 
 function startClock () {
-    // clock.innerHTML = '';
     timer = setInterval(function() {
     seconds++
     clock.innerHTML= createSeconds(seconds);
     }, 1000);
-}
+};
+
 function pauseClock() {
     clearInterval(timer);
-}
+};
+
 function resetClock() {
     clearInterval(timer);
     seconds = 0
     clock.innerHTML = "00:00:00";
-}
+};
+
 function restartFunc() {
     clearInterval(timer);
     seconds = 0;
